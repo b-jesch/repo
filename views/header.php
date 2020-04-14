ac4479df181d324553b7baae84d18ed97baeeed8
b-jesch <birger.jesch@gmail.com>
2020-04-14 12:11:42 +0200

    Cosmetics #2

--- views/header.php
+++ views/header.php
@@ -2,14 +2,15 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo REPONAME.' '.REPOVERSION;?></title>
     <link rel="stylesheet" href="<?php echo ROOT.CSS; ?>" />
     <script src="<?php echo ROOT.FUNCTIONS.'functions.js'; ?>"></script>
 </head>
 <body>
 <hr class="spacer">
-<div>
-<div class="banner" onclick="document.location.href='https://maerz.media';"></div>
+
+<div class="banner" onclick="document.location.href='https://maerz.media';">
 <div class="banner_txt">
         <h1><?php echo REPONAME.' '.REPOVERSION; ?></h1>
         <h2><?php echo 'Tree: '.substr($_SESSION['version'], 0, -1);
