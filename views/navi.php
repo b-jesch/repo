e85bbd6933dc726de6addd445fa92cb241a53776
b-jesch <birger.jesch@gmail.com>
2020-05-01 17:54:12 +0200

    Introducing selection of maintainer addons if user has admin status

--- views/navi.php
+++ views/navi.php
@@ -17,7 +17,21 @@
            title="Download and install the '<?php echo REPONAME; ?>' Addon to receive automatic Updates of all Addons from this Repo">Download Repo Addon</a></li>
     <?php
         if ($_SESSION['state'] == 1) {
-            echo "<li><a href=\"".ROOT.CONTROLLER.'?action=list&user='.$_SESSION['user']."\" title='My Addons'>Meine Addons</a></li>".PHP_EOL;
+            if ($_SESSION['isadmin']) {
+                $users = new User();
+                echo '<li class="dropdown">';
+                echo PHP_TAB.'<a href="javascript:void(0)" class="dropbtn">Maintainer Addons</a>';
+                echo PHP_TAB.'<div class="dropdown-content">';
+
+                $usr_list = $users->getallusers(false);
+                foreach($usr_list as $usr) {
+                    echo "<a href='".ROOT.CONTROLLER."?action=list&user=".$usr."'>$usr</a>".PHP_EOL;
+                }
+                echo "</div></li>";
+
+            } else {
+                echo "<li><a href=\"".ROOT.CONTROLLER.'?action=list&user='.$_SESSION['user']."\" title='My Addons'>Meine Addons</a></li>".PHP_EOL;
+            }
             echo "<li><a href=\"".ROOT.CONTROLLER.'?action=upload'."\" title='Addon Upload'>Upload</a></li>".PHP_EOL;
             echo "<li><a href=\"".ROOT.CONTROLLER.'?action=setup'."\" title='My settings and contact data'>Setup</a></li>".PHP_EOL;
             echo "<li style=\"float: right\"><a href=\"".ROOT.CONTROLLER.'?action=logout'."\">Maintainer Logout</a></li>".PHP_EOL;
