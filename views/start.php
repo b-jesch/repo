d24f699dc63205708ac9643e9760132017ff095f
b-jesch <birger.jesch@gmail.com>
2021-06-17 19:09:27 +0200

    Addon type 'kodi.resource.font' added

--- views/start.php
+++ views/start.php
@@ -1,4 +1,3 @@
 <?php
 # start of CMS, must be identical with ROOT definition in config.php (mandantory)
-define('ROOT', 'https://repo.kodinerds.net/');
-
+const ROOT = 'https://repo.kodinerds.net/';
